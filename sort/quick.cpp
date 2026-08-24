c05216e65fd6a65e4512ef1d6630a44362907156
vijay <vijay.25b01010780@abes.ac.in>
2026-08-24 11:24:41 +0530

    sort

---
+++ sort/quick.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int position(int a[], int low, int high) {
+    int pivot = a[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (a  [j] <= pivot) {
+            i++;
+            swap(a[i], a[j]);
+        }
+    }
+    swap(a[i + 1], a[high]);
+    return i + 1;
+}
+
+void quickSort(int a[], int low, int high) {
+    if (low < high) {
+        int pi = position(a, low, high);
+
+        quickSort(a, low, pi - 1);
+        quickSort(a, pi + 1, high);
+    }
+}
+
+int main() {
+    int a[] = {1,3,4,7,9,8,2,3,4,6};
+    int n = sizeof(a) / sizeof(a[0]);
+
+    quickSort(a, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+
+    return 0;
+}
